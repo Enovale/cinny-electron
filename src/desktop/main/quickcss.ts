957098ed1ca778e1181eab3b049644a10ba2f9a4
Enova <17408285+Enovale@users.noreply.github.com>
2025-10-29 14:58:28 -0500

    Packaging finishing touches

--- src/desktop/main/quickcss.ts
+++ src/desktop/main/quickcss.ts
@@ -1,4 +1,4 @@
-import { readFileSync, watch } from 'fs'
+import { readFileSync, watch, writeFileSync, existsSync, WatchEventType } from 'fs'
 import { join } from 'path'
 import { mainWindow } from './index'
 import { IpcEvents } from '@cinny-electron/core'
@@ -8,11 +8,14 @@ const filePath = join(dataDir, 'quickCSS.css')
 
 export function startQuickCSSWatch(): void {
   console.log('Starting quickcss')
+  if (!existsSync(filePath)) {
+    writeFileSync(filePath, '')
+  }
   watch(filePath, watchCallback)
   watchCallback(null)
 }
 
-function watchCallback(e): void {
+function watchCallback(e: WatchEventType | null): void {
   console.log('quickcss access: ', e)
   mainWindow?.webContents.send(IpcEvents.QUICKCSS_CHANGED, readFileSync(filePath).toString())
 }
